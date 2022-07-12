8c5ef4d3d8f7c4890da5a2f157a4093aba60c269
Gelson Souza <gelson.majorit@gmail.com>
2022-07-12 15:28:45 -0300

    configuracao conexao banco

--- bin/base_backend.dart
+++ bin/base_backend.dart
@@ -1,3 +1,20 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:mysql1/mysql1.dart';
+
+void main(List<String> arguments) async {
+  final conn = await MySqlConnection.connect(ConnectionSettings(
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      db: 'delivery',
+      password: 'root'));
+
+  await conn
+      .query("INSERT INTO tb_permissions (name, status) values ('nacme', '1')");
+
+  print(await conn.query('select * from tb_permissions'));
+
+  await serve(
+      (Request request) => Response(200, body: 'teste'), 'localhost', 3306);
 }
