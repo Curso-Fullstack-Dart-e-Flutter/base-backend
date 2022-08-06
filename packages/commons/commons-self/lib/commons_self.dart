c6acca66bb44519451f24aef138815ef46a81aa4
Gelson Souza <gelson.majorit@gmail.com>
2022-08-06 10:41:35 -0300

    commons-core

---
+++ packages/commons/commons-self/lib/commons_self.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library commons_self;
+
+export 'src/commons_self_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
